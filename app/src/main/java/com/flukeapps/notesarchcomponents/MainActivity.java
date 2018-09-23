67203c56af504a0b263730c4f9860dc95fa84ff0
cojoc07 <cojoc07@gmail.com>
2018-09-23 22:52:20 +0300

    Initial commit

---
+++ app/src/main/java/com/flukeapps/notesarchcomponents/MainActivity.java
@@ -0,0 +1,29 @@
+package com.flukeapps.notesarchcomponents;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update recyclerview
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/flukeapps/notesarchcomponents/NoteRepository.java
@@ -0,0 +1,97 @@
+package com.flukeapps.notesarchcomponents;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDAO noteDAO;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application){
+        NoteDatabase database = NoteDatabase.getInstance(application);
+
+        noteDAO = database.noteDAO();
+        allNotes = noteDAO.getAllNotes();
+    }
+
+    public void insert(Note note){
+        new InsertNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public void update(Note note){
+        new UpdateNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public void delete(Note note){
+        new DeleteNoteAsyncTask(noteDAO).execute(note);
+    }
+
+    public void deleteAllNotes(){
+        new DeleteAllNotesAsyncTask(noteDAO).execute();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
+    }
+
+    private static class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDAO noteDao;
+
+        private InsertNoteAsyncTask(NoteDAO noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDAO noteDao;
+
+        private UpdateNoteAsyncTask(NoteDAO noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDAO noteDao;
+
+        private DeleteNoteAsyncTask(NoteDAO noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDAO noteDao;
+
+        private DeleteAllNotesAsyncTask(NoteDAO noteDao) {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+}
+
