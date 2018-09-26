03c7051ff871b81cf992514cafe7ab633d467fc4
cojoc07 <cojoc07@gmail.com>
2018-09-26 23:21:50 +0300

    added recyclerview
    added recyclerview

--- app/src/main/java/com/flukeapps/notesarchcomponents/MainActivity.java
+++ app/src/main/java/com/flukeapps/notesarchcomponents/MainActivity.java
@@ -5,7 +5,10 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.flukeapps.notesarchcomponents.adapter.NoteAdapter;
 import com.flukeapps.notesarchcomponents.model.Note;
 import com.flukeapps.notesarchcomponents.model.NoteViewModel;
 
@@ -14,18 +17,25 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private NoteViewModel noteViewModel;
+    private RecyclerView recyclerView;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
 
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                //update recyclerview
+                noteAdapter.setNotes(notes);
             }
         });
     }
